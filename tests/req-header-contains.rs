7bd0b13b6081a5e3e9b988eb2e95266314c18d8b
beltram.maldant <beltram.maldant@soprabanking.com>
2020-11-03 14:14:41 +0100

    test: verify more cases for header contains matching

--- tests/req-header-contains.rs
+++ tests/req-header-contains.rs
@@ -14,7 +14,7 @@ fn should_not_default_to_contains() {
 
 #[test]
 fn should_support_contains() {
-    let server = mount("req/headers/contains");
+    let server = mount("req/headers/contains/single");
     let response =
         block_on(surf::get(&server.uri()).set_header("Content-Type", "application/json")).unwrap();
     assert_eq!(response.status().as_u16(), 200);
@@ -22,8 +22,43 @@ fn should_support_contains() {
 
 #[test]
 fn should_fail_when_does_not_contain() {
-    let server = mount("req/headers/contains");
+    let server = mount("req/headers/contains/single");
     let response =
         block_on(surf::get(&server.uri()).set_header("Content-Type", "application/xml")).unwrap();
     assert_eq!(response.status().as_u16(), 404);
+}
+
+#[test]
+fn should_support_many_contains() {
+    let server = mount("req/headers/contains/many");
+    let response =
+        block_on(surf::get(&server.uri())
+            .set_header("Content-Type", "application/json")
+            .set_header("Accept", "application/json")
+        ).unwrap();
+    assert_eq!(response.status().as_u16(), 200);
+}
+
+#[test]
+fn should_support_contains_begin() {
+    let server = mount("req/headers/contains/begin");
+    let response =
+        block_on(surf::get(&server.uri()).set_header("Content-Type", "application/json")).unwrap();
+    assert_eq!(response.status().as_u16(), 200);
+}
+
+#[test]
+fn should_support_contains_middle() {
+    let server = mount("req/headers/contains/middle");
+    let response =
+        block_on(surf::get(&server.uri()).set_header("Content-Type", "application/json")).unwrap();
+    assert_eq!(response.status().as_u16(), 200);
+}
+
+#[test]
+fn should_support_contains_end() {
+    let server = mount("req/headers/contains/end");
+    let response =
+        block_on(surf::get(&server.uri()).set_header("Content-Type", "application/json")).unwrap();
+    assert_eq!(response.status().as_u16(), 200);
 }
