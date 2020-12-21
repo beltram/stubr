f465b0c65672e6aa23efd3dcb751dee27ec33378
beltram.maldant <beltram.maldant@soprabanking.com>
2020-12-21 13:57:36 +0100

    perf: with wiremock 0.4.1

--- bench/scenario.sh
+++ bench/scenario.sh
@@ -7,10 +7,12 @@ scenario() {
   echo "
 |  scenario (duration / users) | avg latency (+/-) | avg req/sec (+/-) | total req | total bytes | avg cpu | avg mem(stubr Kb/wiremock Mb) |
 |:----------------------------:|:-----------------:|:-----------------:|:---------:|:-----------:|:-------:|:-----------------------------:|" >>$OUTPUT
-  stubr_bench "$path" $duration 10
-  wiremock_bench "$path" $duration 10
-  stubr_bench "$path" $duration 100
-  wiremock_bench "$path" $duration 100
-  stubr_bench "$path" $duration 200
-  wiremock_bench "$path" $duration 200
+  stubr_bench "$path" "$duration" 1
+  wiremock_bench "$path" "$duration" 1
+  stubr_bench "$path" "$duration" 10
+  wiremock_bench "$path" "$duration" 10
+  stubr_bench "$path" "$duration" 100
+  wiremock_bench "$path" "$duration" 100
+  stubr_bench "$path" "$duration" 200
+  wiremock_bench "$path" "$duration" 200
 }
