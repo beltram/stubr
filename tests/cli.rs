531ceb01dc205a93dc25f4258d8157d47fb9bef7
beltram <beltram.maldant@soprabanking.com>
2020-12-31 17:06:20 +0100

    test: better cli assertions thanks to a struct

--- tests/cli.rs
+++ tests/cli.rs
@@ -10,26 +10,41 @@ use crate::utils::*;
 
 mod utils;
 
-static PORT: AtomicU16 = AtomicU16::new(60_000);
-
-fn port() -> String {
-    PORT.fetch_add(1, Ordering::SeqCst).to_string()
+struct StubrCli {
+    child: Child,
+    pub addr: String,
 }
 
-fn stubr(args: &[&str]) -> (Child, String) {
-    let port = port();
-    let addr = format!("http://127.0.0.1:{}", &port);
-    let child = Command::cargo_bin("stubr").unwrap()
-        .args(args)
-        .args(&["--port", &port])
-        .spawn().unwrap();
-    sleep(Duration::from_millis(200));
-    (child, addr)
+impl StubrCli {
+    const SLEEP: u64 = 200;
+    const DEFAULT_PORT: u16 = 60_000;
+    const PORT: AtomicU16 = AtomicU16::new(Self::DEFAULT_PORT);
+    const HOST: &'static str = "127.0.0.1";
+
+    fn new(args: &[&str]) -> Self {
+        let port = Self::port();
+        let addr = format!("http://{}:{}", Self::HOST, &port);
+        let child = Command::cargo_bin("stubr").unwrap()
+            .args(args)
+            .args(&["--port", &port])
+            .spawn().unwrap();
+        sleep(Duration::from_millis(Self::SLEEP));
+        Self { child, addr }
+    }
+
+    fn port() -> String {
+        Self::PORT.fetch_add(1, Ordering::SeqCst).to_string()
+    }
+}
+
+impl Drop for StubrCli {
+    fn drop(&mut self) {
+        self.child.kill().unwrap()
+    }
 }
 
 #[async_std::test]
 async fn should_serve_stubs_under_dir() {
-    let (mut child, addr) = stubr(&["tests/stubs/cli"]);
-    get(addr).await.unwrap().assert_ok();
-    child.kill().unwrap()
+    let stubr = StubrCli::new(&["tests/stubs/cli"]);
+    get(&stubr.addr).await.unwrap().assert_ok();
 }
