0c4747e49fcb841eac1f05dee2aa17d1e6cc8245
beltram <beltram.maldant@soprabanking.com>
2021-01-03 16:23:46 +0100

    test: added tests verifying public api

---
+++ tests/api.rs
@@ -0,0 +1,25 @@
+use std::path::{Path, PathBuf};
+
+use surf::get;
+
+use crate::utils::*;
+
+mod utils;
+
+#[async_std::test]
+async fn should_start_server_from_relative_pathbuf() {
+    let srv = Stubr::start(PathBuf::from("tests/stubs/ping.json")).await;
+    get(&srv.uri()).await.unwrap().assert_ok();
+}
+
+#[async_std::test]
+async fn should_start_server_from_relative_path() {
+    let srv = Stubr::start(Path::new("tests/stubs/ping.json")).await;
+    get(&srv.uri()).await.unwrap().assert_ok();
+}
+
+#[async_std::test]
+async fn should_start_server_from_relative_path_as_str() {
+    let srv = Stubr::start("tests/stubs/ping.json").await;
+    get(&srv.uri()).await.unwrap().assert_ok();
+}
