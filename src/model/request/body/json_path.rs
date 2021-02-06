c5e2b7f85edcbf2d760bcca07556bf7d774a8970
beltram.maldant <beltram.maldant@soprabanking.com>
2021-02-06 15:12:38 +0100

    perf: use precompiled json path request matcher to avoid executing expression compilation during request matching

--- src/model/request/body/json_path.rs
+++ src/model/request/body/json_path.rs
@@ -1,17 +1,17 @@
 use std::convert::TryFrom;
 
-use jsonpath_lib::select as matches_json_path;
+use jsonpath_lib::Compiled;
 use serde_json::{from_slice as deserialize, Value};
 use wiremock::{Match, Request};
 
 use super::BodyPatternDto;
 
-pub struct JsonPathMatcher(String);
+pub struct JsonPathMatcher(Compiled);
 
 impl Match for JsonPathMatcher {
     fn matches(&self, req: &Request) -> bool {
         deserialize::<Value>(&req.body).ok().as_ref()
-            .and_then(|it| matches_json_path(it, self.0.as_ref()).ok())
+            .and_then(|it| self.0.select(it).ok())
             .filter(|it| !it.is_empty())
             .is_some()
     }
@@ -23,7 +23,8 @@ impl TryFrom<&BodyPatternDto> for JsonPathMatcher {
     fn try_from(body: &BodyPatternDto) -> anyhow::Result<Self> {
         body.matches_json_path.as_ref()
             .filter(|_| body.is_by_json_path())
-            .map(|it| JsonPathMatcher(it.to_string()))
+            .and_then(|it| jsonpath_lib::Compiled::compile(it.as_str()).ok())
+            .map(|it| JsonPathMatcher(it))
             .ok_or_else(|| anyhow::Error::msg("No json path matcher found"))
     }
 }
