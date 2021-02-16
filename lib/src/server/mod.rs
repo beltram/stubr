4cf255c9b575aac3c4050328fceda653398d541e
beltram.maldant <beltram.maldant@soprabanking.com>
2021-02-16 18:58:48 +0100

    chore: avoid collecting in 'find_all_mocks'

--- lib/src/server/mod.rs
+++ lib/src/server/mod.rs
@@ -69,10 +69,9 @@ impl Stubr {
         } else { vec![] }
     }
 
-    fn find_all_mocks(&self, from: &PathBuf) -> Vec<(Mock, PathBuf)> {
+    fn find_all_mocks(&self, from: &PathBuf) -> impl Iterator<Item=(Mock, PathBuf)> {
         self.find_all_files(from).into_iter()
             .flat_map(|path| StubrMock::try_from(&path).map(|mock| (mock.0, path)))
-            .collect_vec()
     }
 }
 
@@ -119,30 +118,30 @@ mod server_test {
     async fn should_find_all_mocks_from_dir() {
         let from = PathBuf::from("tests/stubs/server");
         let mocks = Stubr::start_on_random_port().await.find_all_mocks(&from);
-        assert_eq!(mocks.len(), 2);
+        assert_eq!(mocks.count(), 2);
     }
 
     #[async_std::test]
     async fn should_find_all_mocks_from_single_file() {
         let from = PathBuf::from("tests/stubs/server/valid.json");
         let mocks = Stubr::start_on_random_port().await.find_all_mocks(&from);
-        assert_eq!(mocks.len(), 1);
+        assert_eq!(mocks.count(), 1);
     }
 
     #[async_std::test]
     async fn should_not_find_any_mock_when_none_valid() {
         let from = PathBuf::from("tests/stubs/server/invalid");
         let mocks = Stubr::start_on_random_port().await.find_all_mocks(&from);
-        assert!(mocks.is_empty());
+        assert_eq!(mocks.count(), 0);
     }
 
     #[async_std::test]
     async fn should_not_find_any_mock_when_path_does_not_exist() {
         let from = PathBuf::from("tests/stubs/server/unknown");
         let mocks = Stubr::start_on_random_port().await.find_all_mocks(&from);
-        assert!(mocks.is_empty());
+        assert_eq!(mocks.count(), 0);
         let from = PathBuf::from("tests/stubs/server/unknown.json");
         let mocks = Stubr::start_on_random_port().await.find_all_mocks(&from);
-        assert!(mocks.is_empty());
+        assert_eq!(mocks.count(), 0);
     }
 }
