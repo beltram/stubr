888b0fc8e1cb33516b1cfa0267fcecfa195a5732
beltram.maldant <beltram.maldant@soprabanking.com>
2021-03-11 20:52:40 +0100

    tests: split config tests into 2 files

--- lib/tests/config.rs
+++ lib/tests/config.rs
@@ -26,25 +26,3 @@ async fn should_start_server_in_a_blocking_way_with_some_configuration() {
     assert_eq!(srv.uri().as_str(), expected_uri);
     get(expected_uri).await.unwrap().assert_ok();
 }
-
-#[async_std::test]
-async fn should_timeout_with_global_delay_of_2_seconds() {
-    let cfg = Config { global_delay: Some(2000), ..Default::default() };
-    let srv = Stubr::start_with("tests/stubs/ping.json", cfg).await;
-    let timeout = task::block_on(io::timeout(Duration::from_secs(1), async {
-        get(&srv.url()).await.unwrap().assert_ok();
-        Ok(())
-    }));
-    assert!(timeout.is_err())
-}
-
-#[async_std::test]
-async fn should_not_timeout_with_global_delay_of_2_seconds() {
-    let cfg = Config { global_delay: Some(2000), ..Default::default() };
-    let srv = Stubr::start_with("tests/stubs/ping.json", cfg).await;
-    let timeout = task::block_on(io::timeout(Duration::from_secs(3), async {
-        get(&srv.url()).await.unwrap().assert_ok();
-        Ok(())
-    }));
-    assert!(timeout.is_ok())
-}
