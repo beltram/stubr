03028c0c4374a586ddaf1bc0a3c0eb3dc72289fb
beltram.maldant <beltram.maldant@soprabanking.com>
2021-07-06 19:04:01 +0200

    test: expect recorder to be startable on dedicated port

--- lib/tests/record/config.rs
+++ lib/tests/record/config.rs
@@ -18,3 +18,11 @@ async fn should_allow_custom_output() {
     Stubr::record_with(cfg).isahc_client().get(stubr.path("/status/200")).expect_status_ok();
     assert!(output.join("localhost").join("status-200-1330526116653087821.json").exists())
 }
+
+#[tokio::test(flavor = "multi_thread")]
+#[stubr::mock("record/status/200.json")]
+async fn should_allow_recording_on_dedicated_port() {
+    let recorder = Stubr::record_with(RecordConfig { port: Some(1234), ..record_cfg() });
+    recorder.isahc_client().get(stubr.path("/status/200")).expect_status_ok();
+    assert_eq!(recorder.uri(), String::from("http://127.0.0.1:1234"))
+}
