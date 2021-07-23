2f703ee0f89e56d0354939d883e4ecbe3f40df4e
beltram.maldant <beltram.maldant@soprabanking.com>
2021-07-23 14:39:23 +0200

    fix: 'record' in cli did not work due to premature drop called

--- cli/src/record/mod.rs
+++ cli/src/record/mod.rs
@@ -11,7 +11,7 @@ impl Record {
     const SLEEP_DURATION: Duration = Duration::from_millis(1000);
 
     pub async fn record(config: CliRecordConfig) {
-        Stubr::record_with(config.into());
+        let _recorder = Stubr::record_with(config.into());
         loop { async_std::task::sleep(Self::SLEEP_DURATION).await; }
     }
 }
