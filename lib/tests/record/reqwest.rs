b7f5698148589ee5bec9e8f92a3f4941a79419a6
gmazollier <guillaume.mazollier@soprabanking.com>
2021-08-06 17:56:39 +0200

    feat: test suite for record_client for Reqwest

--- lib/tests/record/reqwest.rs
+++ lib/tests/record/reqwest.rs
@@ -1,4 +1,3 @@
-use asserhttp::*;
 use reqwest::blocking::ClientBuilder as ReqwestBlockingClientBuilder;
 use serde_json::json;
 
@@ -7,7 +6,7 @@ use stubr::{Record, RecordConfig};
 use crate::utils::*;
 
 #[test]
-#[stubr::mock("record/record-client/")]
+#[stubr::mock("record/record-client")]
 fn should_record_from_reqwest_client() {
     let cfg = RecordConfig {
         except_request_headers: Some(relaxed_req_headers()),
@@ -25,6 +24,31 @@ fn should_record_from_reqwest_client() {
                                         },
                                         "response": {
                                             "status": 200
+                                        }
+                                    }
+    ))
+}
+
+
+#[test]
+#[stubr::mock("record/record-client")]
+fn should_record_from_reqwest_client_ko() {
+    let cfg = RecordConfig {
+        except_request_headers: Some(relaxed_req_headers()),
+        except_response_headers: Some(relaxed_resp_headers()),
+        ..Default::default()
+    };
+    ReqwestBlockingClientBuilder::new().build().unwrap()
+        .get(stubr.path("/record-client/reqwest/ko"))
+        .record_with(cfg);
+    assert_recorded_stub_eq("record-client-reqwest-ko-3875024637254819038", json!(
+                                    {
+                                        "request": {
+                                            "method": "GET",
+                                            "urlPath": "/record-client/reqwest/ko"
+                                        },
+                                        "response": {
+                                            "status": 404
                                         }
                                     }
     ))
