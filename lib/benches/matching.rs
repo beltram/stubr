ce6a37c2e9364c90efee84e38b681ac738c26f2a
beltram.maldant <beltram.maldant@soprabanking.com>
2021-12-17 15:21:38 +0100

    bench: more request matching benches

--- lib/benches/matching.rs
+++ lib/benches/matching.rs
@@ -1,5 +1,6 @@
 use criterion::{async_executor::AsyncStdExecutor, black_box, Criterion, criterion_group, criterion_main};
-use surf::get;
+use serde_json::json;
+use surf::{get, post};
 
 use stubr::Stubr;
 
@@ -47,9 +48,50 @@ fn query_bench(c: &mut Criterion) {
     });
 }
 
+fn body_bench(c: &mut Criterion) {
+    let srv = Stubr::start_blocking("benches/stubs/matching/body");
+    let eq_to_json_uri = format!("{}{}", srv.uri(), "/api/body-equal-to-json");
+    c.bench_function("matching body 'equalToJson'", |b| {
+        b.to_async(AsyncStdExecutor).iter(|| black_box(post(&eq_to_json_uri).body(json!({"name":"john.doe"}))))
+    });
+    let eq_to_json_extra_uri = format!("{}{}", srv.uri(), "/api/body-equal-to-json-extra");
+    c.bench_function("matching body 'equalToJson' ignoring extra elements", |b| {
+        b.to_async(AsyncStdExecutor).iter(|| black_box(post(&eq_to_json_extra_uri).body(json!({"name":"john.doe", "age": 42}))))
+    });
+    let eq_to_json_order_uri = format!("{}{}", srv.uri(), "/api/body-equal-to-json-order");
+    c.bench_function("matching body 'equalToJson' ignoring array order", |b| {
+        b.to_async(AsyncStdExecutor).iter(|| black_box(post(&eq_to_json_order_uri).body(json!({"names":["doe", "john"]}))))
+    });
+    let matches_json_path_uri = format!("{}{}", srv.uri(), "/api/matches-json-path");
+    c.bench_function("matching body single json path", |b| {
+        b.to_async(AsyncStdExecutor).iter(|| black_box(post(&matches_json_path_uri).body(json!({"name":"john.doe"}))))
+    });
+    let matches_many_json_path_uri = format!("{}{}", srv.uri(), "/api/matches-many-json-path");
+    c.bench_function("matching body many json path", |b| {
+        b.to_async(AsyncStdExecutor).iter(|| black_box(post(&matches_many_json_path_uri).body(json!({"a":1,"b":2,"c":3,"d":4,"e":5,"f":6,"g":7,"h":8,"i":9,"j":10,}))))
+    });
+    let matches_json_path_eq_uri = format!("{}{}", srv.uri(), "/api/matches-json-path-eq");
+    c.bench_function("matching body json path eq", |b| {
+        b.to_async(AsyncStdExecutor).iter(|| black_box(post(&matches_json_path_eq_uri).body(json!({"consoles": [ { "id": "xbox" } ]}))))
+    });
+    let matches_json_path_gt_uri = format!("{}{}", srv.uri(), "/api/matches-json-path-gt");
+    c.bench_function("matching body json path comparison", |b| {
+        b.to_async(AsyncStdExecutor).iter(|| black_box(post(&matches_json_path_gt_uri).body(json!({"consoles": [ { "price": 201 } ]}))))
+    });
+    let matches_expression_eq_uri = format!("{}{}", srv.uri(), "/api/expression-eq");
+    c.bench_function("matching body json path expression equals", |b| {
+        b.to_async(AsyncStdExecutor).iter(|| black_box(post(&matches_expression_eq_uri).body(json!({"person": { "name": "john.doe" }}))))
+    });
+    let matches_expression_contains_uri = format!("{}{}", srv.uri(), "/api/expression-contains");
+    c.bench_function("matching body json path expression contains", |b| {
+        b.to_async(AsyncStdExecutor).iter(|| black_box(post(&matches_expression_contains_uri).body(json!({"person": { "name": "john.doe" }}))))
+    });
+}
+
 fn matching_bench(c: &mut Criterion) {
     url_bench(c);
     query_bench(c);
+    body_bench(c);
 }
 
 criterion_group!(matching, matching_bench);
