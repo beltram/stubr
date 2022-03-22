333ce0df9c045d2e5664a73c055541dd31f11167
beltram.maldant <beltram.maldant@soprabanking.com>
2022-03-22 18:57:50 +0100

    feat: verify supports response templating !

--- lib/src/verify/actix/req.rs
+++ lib/src/verify/actix/req.rs
@@ -1,13 +1,12 @@
 use std::str::FromStr;
 
 use actix_web::test::TestRequest;
-use async_std::task::block_on;
 use http::Method as HttpMethod;
 
 use super::super::mapping::req::StdRequest;
 
-impl From<&mut StdRequest> for TestRequest {
-    fn from(req: &mut StdRequest) -> Self {
+impl From<&StdRequest> for TestRequest {
+    fn from(req: &StdRequest) -> Self {
         let method = HttpMethod::from_str(req.0.method().as_ref()).expect("Unknown http method");
         let mut test_req = Self::default();
         let original_headers = req.0.header_names().into_iter()
@@ -15,10 +14,7 @@ impl From<&mut StdRequest> for TestRequest {
         for (k, v) in original_headers {
             test_req = test_req.insert_header((k.as_str(), v.as_str()));
         }
-        test_req
-            .method(method)
-            .uri(req.0.url().as_str())
-            .set_payload(block_on(async move { req.0.body_bytes().await.unwrap() }))
+        test_req.method(method).uri(req.0.url().as_str())
     }
 }
 
@@ -34,56 +30,56 @@ mod actix_req_mapping_tests {
 
         #[test]
         fn should_map_get() {
-            let mut req = StdRequest(Request::get("http://localhost/"));
-            assert_eq!(TestRequest::from(&mut req).to_http_request().method(), Method::GET)
+            let req = StdRequest(Request::get("http://localhost/"));
+            assert_eq!(TestRequest::from(&req).to_http_request().method(), Method::GET)
         }
 
         #[test]
         fn should_map_post() {
-            let mut req = StdRequest(Request::post("http://localhost/"));
-            assert_eq!(TestRequest::from(&mut req).to_http_request().method(), Method::POST)
+            let req = StdRequest(Request::post("http://localhost/"));
+            assert_eq!(TestRequest::from(&req).to_http_request().method(), Method::POST)
         }
 
         #[test]
         fn should_map_put() {
-            let mut req = StdRequest(Request::put("http://localhost/"));
-            assert_eq!(TestRequest::from(&mut req).to_http_request().method(), Method::PUT)
+            let req = StdRequest(Request::put("http://localhost/"));
+            assert_eq!(TestRequest::from(&req).to_http_request().method(), Method::PUT)
         }
 
         #[test]
         fn should_map_delete() {
-            let mut req = StdRequest(Request::delete("http://localhost/"));
-            assert_eq!(TestRequest::from(&mut req).to_http_request().method(), Method::DELETE)
+            let req = StdRequest(Request::delete("http://localhost/"));
+            assert_eq!(TestRequest::from(&req).to_http_request().method(), Method::DELETE)
         }
 
         #[test]
         fn should_map_patch() {
-            let mut req = StdRequest(Request::patch("http://localhost/"));
-            assert_eq!(TestRequest::from(&mut req).to_http_request().method(), Method::PATCH)
+            let req = StdRequest(Request::patch("http://localhost/"));
+            assert_eq!(TestRequest::from(&req).to_http_request().method(), Method::PATCH)
         }
 
         #[test]
         fn should_map_options() {
-            let mut req = StdRequest(Request::options("http://localhost/"));
-            assert_eq!(TestRequest::from(&mut req).to_http_request().method(), Method::OPTIONS)
+            let req = StdRequest(Request::options("http://localhost/"));
+            assert_eq!(TestRequest::from(&req).to_http_request().method(), Method::OPTIONS)
         }
 
         #[test]
         fn should_map_head() {
-            let mut req = StdRequest(Request::head("http://localhost/"));
-            assert_eq!(TestRequest::from(&mut req).to_http_request().method(), Method::HEAD)
+            let req = StdRequest(Request::head("http://localhost/"));
+            assert_eq!(TestRequest::from(&req).to_http_request().method(), Method::HEAD)
         }
 
         #[test]
         fn should_map_trace() {
-            let mut req = StdRequest(Request::trace("http://localhost/"));
-            assert_eq!(TestRequest::from(&mut req).to_http_request().method(), Method::TRACE)
+            let req = StdRequest(Request::trace("http://localhost/"));
+            assert_eq!(TestRequest::from(&req).to_http_request().method(), Method::TRACE)
         }
 
         #[test]
         fn should_map_connect() {
-            let mut req = StdRequest(Request::connect("http://localhost/"));
-            assert_eq!(TestRequest::from(&mut req).to_http_request().method(), Method::CONNECT)
+            let req = StdRequest(Request::connect("http://localhost/"));
+            assert_eq!(TestRequest::from(&req).to_http_request().method(), Method::CONNECT)
         }
     }
 
@@ -94,26 +90,26 @@ mod actix_req_mapping_tests {
 
         #[test]
         fn should_map_scheme() {
-            let mut req = StdRequest(Request::get("http://github.com:8080/api/url"));
-            assert_eq!(TestRequest::from(&mut req).to_http_request().uri().scheme(), Some(&Scheme::HTTP));
+            let req = StdRequest(Request::get("http://github.com:8080/api/url"));
+            assert_eq!(TestRequest::from(&req).to_http_request().uri().scheme(), Some(&Scheme::HTTP));
         }
 
         #[test]
         fn should_map_host() {
-            let mut req = StdRequest(Request::get("http://github.com:8080/api/url"));
-            assert_eq!(TestRequest::from(&mut req).to_http_request().uri().host(), Some("github.com"));
+            let req = StdRequest(Request::get("http://github.com:8080/api/url"));
+            assert_eq!(TestRequest::from(&req).to_http_request().uri().host(), Some("github.com"));
         }
 
         #[test]
         fn should_map_port() {
-            let mut req = StdRequest(Request::get("http://github.com:8080/api/url"));
-            assert_eq!(TestRequest::from(&mut req).to_http_request().uri().port_u16(), Some(8080));
+            let req = StdRequest(Request::get("http://github.com:8080/api/url"));
+            assert_eq!(TestRequest::from(&req).to_http_request().uri().port_u16(), Some(8080));
         }
 
         #[test]
         fn should_map_path() {
-            let mut req = StdRequest(Request::get("http://github.com:8080/api/url"));
-            assert_eq!(TestRequest::from(&mut req).to_http_request().uri().path(), "/api/url");
+            let req = StdRequest(Request::get("http://github.com:8080/api/url"));
+            assert_eq!(TestRequest::from(&req).to_http_request().uri().path(), "/api/url");
         }
     }
 
@@ -122,20 +118,20 @@ mod actix_req_mapping_tests {
 
         #[test]
         fn should_map_one() {
-            let mut req = StdRequest(Request::get("http://localhost?a=b"));
-            assert_eq!(TestRequest::from(&mut req).to_http_request().uri().query(), Some("a=b"));
+            let req = StdRequest(Request::get("http://localhost?a=b"));
+            assert_eq!(TestRequest::from(&req).to_http_request().uri().query(), Some("a=b"));
         }
 
         #[test]
         fn should_map_many() {
-            let mut req = StdRequest(Request::get("http://localhost?a=b&c=d"));
-            assert_eq!(TestRequest::from(&mut req).to_http_request().uri().query(), Some("a=b&c=d"));
+            let req = StdRequest(Request::get("http://localhost?a=b&c=d"));
+            assert_eq!(TestRequest::from(&req).to_http_request().uri().query(), Some("a=b&c=d"));
         }
 
         #[test]
         fn should_not_fail_when_none() {
-            let mut req = StdRequest(Request::get("http://localhost"));
-            assert!(TestRequest::from(&mut req).to_http_request().uri().query().is_none());
+            let req = StdRequest(Request::get("http://localhost"));
+            assert!(TestRequest::from(&req).to_http_request().uri().query().is_none());
         }
     }
 
@@ -148,7 +144,7 @@ mod actix_req_mapping_tests {
         fn should_map_one() {
             let mut req = Request::get("http://localhost");
             req.append_header("x-a", "b");
-            let test_req = TestRequest::from(&mut StdRequest(req)).to_http_request();
+            let test_req = TestRequest::from(&StdRequest(req)).to_http_request();
             assert_eq!(test_req.headers().get("x-a"), Some(&HeaderValue::from_str("b").unwrap()));
         }
 
@@ -157,7 +153,7 @@ mod actix_req_mapping_tests {
             let mut req = Request::get("http://localhost");
             req.append_header("x-a", "b");
             req.append_header("x-c", "d");
-            let test_req = TestRequest::from(&mut StdRequest(req)).to_http_request();
+            let test_req = TestRequest::from(&StdRequest(req)).to_http_request();
             assert_eq!(test_req.headers().get("x-a"), Some(&HeaderValue::from_str("b").unwrap()));
             assert_eq!(test_req.headers().get("x-c"), Some(&HeaderValue::from_str("d").unwrap()));
         }
@@ -166,7 +162,7 @@ mod actix_req_mapping_tests {
         fn should_map_multi() {
             let mut req = Request::get("http://localhost");
             req.append_header("x-a", "b, c");
-            let test_req = TestRequest::from(&mut StdRequest(req)).to_http_request();
+            let test_req = TestRequest::from(&StdRequest(req)).to_http_request();
             let mut values = test_req.headers().get_all("x-a");
             assert_eq!(values.next(), Some(&HeaderValue::from_str("b, c").unwrap()));
         }
@@ -174,40 +170,9 @@ mod actix_req_mapping_tests {
 
         #[test]
         fn should_not_fail_when_none() {
-            let mut req = StdRequest(Request::get("http://localhost"));
-            let test_req = TestRequest::from(&mut req).to_http_request();
+            let req = StdRequest(Request::get("http://localhost"));
+            let test_req = TestRequest::from(&req).to_http_request();
             assert!(test_req.headers().is_empty());
-        }
-    }
-
-    mod body {
-        use actix_http::HttpMessage;
-        use futures::TryStreamExt;
-
-        use super::*;
-
-        #[async_std::test]
-        async fn should_map_body() {
-            let input = "ACAB".as_bytes();
-            let mut req = Request::post("http://localhost");
-            req.set_body(input);
-            let mut test_req = TestRequest::from(&mut StdRequest(req)).to_srv_request();
-            let mut body = vec![];
-            while let Ok(Some(chunk)) = test_req.take_payload().try_next().await {
-                body.append(&mut chunk.as_ref().to_vec());
-            }
-            assert_eq!(body.as_slice(), input);
-        }
-
-        #[async_std::test]
-        async fn should_not_fail_when_no_body() {
-            let req = Request::get("http://localhost");
-            let mut test_req = TestRequest::from(&mut StdRequest(req)).to_srv_request();
-            let mut body = vec![];
-            while let Ok(Some(chunk)) = test_req.take_payload().try_next().await {
-                body.append(&mut chunk.as_ref().to_vec());
-            }
-            assert!(body.as_slice().is_empty());
         }
     }
 }
--- lib/src/verify/mapping/resp/status.rs
+++ lib/src/verify/mapping/resp/status.rs
@@ -5,7 +5,7 @@ use super::{StdResponse, super::req::StdRequest, Verifier};
 pub struct StatusVerifier;
 
 impl Verifier<'_> for StatusVerifier {
-    fn verify(stub: &'_ ResponseStub, name: &'_ str, _req: &'_ StdRequest, resp: &'_ mut StdResponse) {
+    fn verify(stub: &'_ ResponseStub, name: &'_ str, _req: &'_ mut StdRequest, resp: &'_ mut StdResponse) {
         let expected = stub.status();
         let actual = u16::from(resp.0.status());
         assert_eq!(actual, expected,
@@ -23,17 +23,17 @@ mod status_verify_tests {
     #[test]
     fn should_verify_200() {
         let stub = ResponseStub { status: Some(200), ..Default::default() };
-        let req = StdRequest(Request::get("http://localhost/"));
+        let mut req = StdRequest(Request::get("http://localhost/"));
         let mut resp = StdResponse(Response::new(200));
-        StatusVerifier::verify(&stub, "200", &req, &mut resp);
+        StatusVerifier::verify(&stub, "200", &mut req, &mut resp);
     }
 
     #[should_panic(expected = "Verification failed for stub '200'. Expected response status to be '200' but was '201'")]
     #[test]
     fn verify_should_fail_when_wrong_status_returned() {
         let stub = ResponseStub { status: Some(200), ..Default::default() };
-        let req = StdRequest(Request::get("http://localhost/"));
+        let mut req = StdRequest(Request::get("http://localhost/"));
         let mut resp = StdResponse(Response::new(201));
-        StatusVerifier::verify(&stub, "200", &req, &mut resp);
+        StatusVerifier::verify(&stub, "200", &mut req, &mut resp);
     }
 }
