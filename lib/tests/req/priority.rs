0aa5a2248f2c4c2e66a619394012f619eca81577
beltram <beltram.maldant@wire.com>
2022-06-10 12:49:16 +0200

    test: remove deprecated use of `expect_status_eq`

--- lib/tests/req/priority.rs
+++ lib/tests/req/priority.rs
@@ -3,11 +3,11 @@ use asserhttp::*;
 #[async_std::test]
 #[stubr::mock("req/priority/exact-high.json", "req/priority/regex-low.json")]
 async fn should_prioritize_exact_as_highest_priority() {
-    surf::get(stubr.path("/abcd")).await.expect_status_eq(200);
+    surf::get(stubr.path("/abcd")).await.expect_status(200);
 }
 
 #[async_std::test]
 #[stubr::mock("req/priority/exact-low.json", "req/priority/regex-high.json")]
 async fn should_prioritize_regex_as_highest_priority() {
-    surf::get(stubr.path("/abcd")).await.expect_status_eq(201);
+    surf::get(stubr.path("/abcd")).await.expect_status(201);
 }
--- lib/tests/resp/status.rs
+++ lib/tests/resp/status.rs
@@ -4,23 +4,23 @@ use surf::get;
 #[async_std::test]
 #[stubr::mock("resp/status/default.json")]
 async fn status_should_default_to_200() {
-    get(stubr.uri()).await.expect_status_eq(200);
+    get(stubr.uri()).await.expect_status(200);
 }
 
 #[async_std::test]
 #[stubr::mock("resp/status/200.json")]
 async fn should_map_response_status_200() {
-    get(stubr.uri()).await.expect_status_eq(200);
+    get(stubr.uri()).await.expect_status(200);
 }
 
 #[async_std::test]
 #[stubr::mock("resp/status/400.json")]
 async fn should_map_response_status_400() {
-    get(stubr.uri()).await.expect_status_eq(400);
+    get(stubr.uri()).await.expect_status(400);
 }
 
 #[async_std::test]
 #[stubr::mock("resp/status/500.json")]
 async fn should_map_response_status_500() {
-    get(stubr.uri()).await.expect_status_eq(500);
+    get(stubr.uri()).await.expect_status(500);
 }
