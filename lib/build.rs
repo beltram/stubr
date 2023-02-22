6c0a8fae2a9ba0869cb901beb2bd029607ebd8c9
beltram <beltram.maldant@gmail.com>
2023-02-22 09:22:53 +0100

    fix: building Docker image for musl

--- lib/build.rs
+++ lib/build.rs
@@ -1,6 +1,10 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    for proto in std::fs::read_dir("tests/grpc/protos").unwrap() {
-        proto.map(|p| p.path()).ok().map(tonic_build::compile_protos).transpose()?;
+    // otherwise fails while building Docker image
+    #[cfg(not(target_env = "musl"))]
+    {
+        for proto in std::fs::read_dir("tests/grpc/protos").unwrap() {
+            proto.map(|p| p.path()).ok().map(tonic_build::compile_protos).transpose()?;
+        }
     }
     Ok(())
 }
