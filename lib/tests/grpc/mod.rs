b5499c43e2172c322e2694f33690f9b7414d6bb9
beltram <beltram.maldant@gmail.com>
2023-04-25 21:02:33 +0200

    feat: fix gRPC method matching and introduce a matcher for the service name

--- lib/tests/grpc/mod.rs
+++ lib/tests/grpc/mod.rs
@@ -14,11 +14,16 @@ pub mod resp;
 #[async_trait::async_trait(? Send)]
 pub trait GrpcConnect {
     async fn connect(&self) -> grpc_client::GrpcClient<tonic::transport::Channel>;
+    async fn connect_other(&self) -> grpc_other_client::GrpcOtherClient<tonic::transport::Channel>;
 }
 
 #[async_trait::async_trait(? Send)]
 impl GrpcConnect for stubr::Stubr {
     async fn connect(&self) -> grpc_client::GrpcClient<tonic::transport::Channel> {
         grpc::grpc_client::GrpcClient::connect(self.uri()).await.unwrap()
+    }
+
+    async fn connect_other(&self) -> grpc_other_client::GrpcOtherClient<tonic::transport::Channel> {
+        grpc::grpc_other_client::GrpcOtherClient::connect(self.uri()).await.unwrap()
     }
 }
