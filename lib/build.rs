df12a8e70743a8f4f714ab3a900167a5a17379ee
beltram <beltram.maldant@gmail.com>
2023-06-13 17:44:33 +0200

    try

--- lib/build.rs
+++ lib/build.rs
@@ -1,9 +1,9 @@
 // otherwise fails while building Docker image
 #[cfg(not(target_env = "musl"))]
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    for proto in std::fs::read_dir("tests/grpc/protos")? {
+    /*for proto in std::fs::read_dir("tests/grpc/protos")? {
         tonic_build::compile_protos(proto?.path())?;
-    }
+    }*/
     Ok(())
 }
 
